06e12e0e33501aa90f0261667e2433916904d593
Uddhav Zambare <114619316+uddhavz@users.noreply.github.com>
2023-10-06 08:44:40 -0400

    Update Architecture.py

--- Part2/pages/Architecture.py
+++ Part2/pages/Architecture.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.header("Architecture Diagram")
+st.markdown("<h1 style='text-align: center;'>Architecture Diagram</h1>", unsafe_allow_html=True)
 st.image("./Part2/architecture_diagram.png", use_column_width=True)
