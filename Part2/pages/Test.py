1e069ee5f4248eab2c68601336c7ed89769c3be9
Uddhav Zambare <114619316+uddhavz@users.noreply.github.com>
2023-10-06 08:14:24 -0400

    Update Test.py

--- Part2/pages/Test.py
+++ Part2/pages/Test.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import os
 
-a = os.getcwd()
-b = os.listdir()
+def list_files_and_directories(path):
+    for root, dirs, files in os.walk(path):
+        print(f"Current directory: {root}")
+        
+        # List all directories in the current directory
+        for directory in dirs:
+            print(f"Directory: {os.path.join(root, directory)}")
+        
+        # List all files in the current directory
+        for file in files:
+            print(f"File: {os.path.join(root, file)}")
 
-st.write(a)
-st.write(b)
+# Specify the directory you want to start from
+start_directory = "."
+list_files_and_directories(start_directory)
+
