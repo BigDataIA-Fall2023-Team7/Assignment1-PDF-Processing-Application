50f0a9c41fe94eb38a09c8a976bc0573e345ed83
Uddhav Zambare <114619316+uddhavz@users.noreply.github.com>
2023-10-06 08:15:24 -0400

    Update Test.py

--- Part2/pages/Test.py
+++ Part2/pages/Test.py
@@ -3,15 +3,15 @@ import os
 
 def list_files_and_directories(path):
     for root, dirs, files in os.walk(path):
-        print(f"Current directory: {root}")
+        st.write(f"Current directory: {root}")
         
         # List all directories in the current directory
         for directory in dirs:
-            print(f"Directory: {os.path.join(root, directory)}")
+            st.write(f"Directory: {os.path.join(root, directory)}")
         
         # List all files in the current directory
         for file in files:
-            print(f"File: {os.path.join(root, file)}")
+            st.write(f"File: {os.path.join(root, file)}")
 
 # Specify the directory you want to start from
 start_directory = "."
