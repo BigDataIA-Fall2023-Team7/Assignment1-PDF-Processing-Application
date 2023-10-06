35774926334a9c8195fded241856a5c21851d231
Aditya Kawale <adityamkawale@gmail.com>
2023-10-06 12:46:22 -0400

    #19 Added the logic for Nougat API Url field

--- Part1/main.py
+++ Part1/main.py
@@ -51,18 +51,19 @@ pdf_link_validator = re.compile(r'https?://\S+$')
 #Header
 st.header("PDF Processing Application")
 
-#Input Nougat API Server URL Textbox
-nougatAPIServerURL = st.text_input("Link to Nougat API Server", value="", max_chars=None, key="nougatAPIServerURL", type="default", help="Enter the link to the Nougat API Server")
-
 #Input PDF Link Textbox
 input_pdf_link = st.text_input("Link to PDF", value="", max_chars=None, key="input_pdf_link", type="default", help="Enter the link to the PDF which you want to send for processing!")
 
 #Select Library Choice Radio Button
 st.radio("Choose the PDF Processor: ",["Nougat", "PyPDF"], captions=["Recommended for physically scanned documents, mathematical equations, etc.", "Recommended for digitally created documents"],index=0, key="input_pdf_processor")
 
+if st.session_state.input_pdf_processor == "Nougat":
+    #Input Nougat API Server URL Textbox
+    nougatAPIServerURL = st.text_input("Link to Nougat API Server", value="", max_chars=None, key="nougatAPIServerURL", type="default", help="Enter the link to the Nougat API Server")
+
 #Process Button
 if st.button("Process!", key="process_button", type='primary'):
-    if input_pdf_link!='' and nougatAPIServerURL!='':
+    if input_pdf_link!='':
         if re.search(pdf_link_validator, input_pdf_link):
             input_pdf_processor = st.session_state.input_pdf_processor
             try:
@@ -71,51 +72,58 @@ if st.button("Process!", key="process_button", type='primary'):
                 if downloaded_pdf_file.status_code == 200:
                     content_type = downloaded_pdf_file.headers.get("Content-Type")
                     if 'application/pdf' in content_type.lower():
-                        st.success("Sending the PDF at '{}' for processing using '{}' processor!".format(input_pdf_link, input_pdf_processor), icon='✅')
+                        
                         if input_pdf_processor == "Nougat":
+
+                            if nougatAPIServerURL!='':
                             
-                            #Record Start time
-                            start_time = time.time()
+                                #Record Start time
+                                start_time = time.time()
 
-                            nougatAPIHeaders = {
-                                "Accept":"application/json"
-                            }
-                            nougatAPIInputPDF = {'file':downloaded_pdf_file.content}
+                                nougatAPIHeaders = {
+                                    "Accept":"application/json"
+                                }
+                                nougatAPIInputPDF = {'file':downloaded_pdf_file.content}
 
-                            processedPdfData = requests.post(nougatAPIServerURL + "/predict", headers=nougatAPIHeaders, files=nougatAPIInputPDF)
-                            
-                            if processedPdfData.status_code == 200:
-                                #preprocess rules for creating mmd file
-                                #1. Unstrigify -> One header label
-                                #2. \n\n -> Actual newline character * 2
-                                #3. \n -> Actual newline character * 1
-                                #4. \\ -> \
-
-                                cleanData = processedPdfData.content[1:-1].decode().replace(r"\n\n",'\n\n').replace(r"\n",'\n').replace('\\\\', '\\')
-
-                                outputFileName = get_output_filename("Nougat")
-
-                                # Record the end time
-                                end_time = time.time()
-
-                                st.success("Processing complete!".format(input_pdf_link, input_pdf_processor), icon='✅')
-                            elif processedPdfData.status_code == 404:
-                                e = RuntimeError("Connection to Nougat API Server lost!")
-                                st.exception(e)
-                            else:
-                                e = RuntimeError(str(processedPdfData.status_code) + " - " + processedPdfData.reason + "got from the API server")
-                                st.exception(e)
+                                st.success("Sending the PDF at '{}' for processing using '{}' processor!".format(input_pdf_link, input_pdf_processor), icon='✅')
+                                processedPdfData = requests.post(nougatAPIServerURL + "/predict", headers=nougatAPIHeaders, files=nougatAPIInputPDF)
                                 
-                            
-                            # Calculate and display the processing time
-                            if cleanData:
-                                processing_time = end_time - start_time
-                                st.subheader("Processing Time:")
-                                st.write(f"Time taken: {processing_time:.2f} seconds")
-                                st.download_button(label="Download the Processed File", data=cleanData, file_name=outputFileName)
+                                if processedPdfData.status_code == 200:
+                                    #preprocess rules for creating mmd file
+                                    #1. Unstrigify -> One header label
+                                    #2. \n\n -> Actual newline character * 2
+                                    #3. \n -> Actual newline character * 1
+                                    #4. \\ -> \
+
+                                    cleanData = processedPdfData.content[1:-1].decode().replace(r"\n\n",'\n\n').replace(r"\n",'\n').replace('\\\\', '\\')
+
+                                    outputFileName = get_output_filename("Nougat")
+
+                                    # Record the end time
+                                    end_time = time.time()
+
+                                    st.success("Processing complete!".format(input_pdf_link, input_pdf_processor), icon='✅')
+                                elif processedPdfData.status_code == 404:
+                                    e = RuntimeError("Connection to Nougat API Server lost! Check if the server is active or if the URL is correct")
+                                    st.exception(e)
+                                else:
+                                    e = RuntimeError(str(processedPdfData.status_code) + " - " + processedPdfData.reason + "got from the API server")
+                                    st.exception(e)
+                                    
+                                
+                                # Calculate and display the processing time
+                                if cleanData:
+                                    processing_time = end_time - start_time
+                                    st.subheader("Processing Time:")
+                                    st.write(f"Time taken: {processing_time:.2f} seconds")
+                                    st.download_button(label="Download the Processed File", data=cleanData, file_name=outputFileName)
+
+                            else:
+                                st.error("Empty Link: Please enter the URL to Nougat API Server", icon='❗️')
 
                         elif input_pdf_processor == "PyPDF":
                             
+                            st.success("Sending the PDF at '{}' for processing using '{}' processor!".format(input_pdf_link, input_pdf_processor), icon='✅')
                             #Record Start time
                             start_time = time.time()
                             
@@ -146,8 +154,6 @@ if st.button("Process!", key="process_button", type='primary'):
         else:
             st.error("Invalid Link: Please check your link {Input format : <http | https>://<server-location>.pdf</>}", icon='❗️')
     else:
-        if nougatAPIServerURL=="":
-            st.error("Empty Link: Please enter the URL to Nougat API Server", icon='❗️')
         if input_pdf_link == "":
             st.error("Empty Link: Please enter a link to pdf in the textbox", icon='❗️')
 
